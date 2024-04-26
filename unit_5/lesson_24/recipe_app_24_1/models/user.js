4218e1e584a163ec56cb759ff590d76494be6dc0
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-04-26 15:41:58 +0200

    lesson_24

--- unit_5/lesson_24/recipe_app_24_1/models/user.js
+++ unit_5/lesson_24/recipe_app_24_1/models/user.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose"),
   { Schema } = mongoose,
   Subscriber = require("./subscriber"),
   bcrypt = require("bcrypt"),
+  passportLocalMongoose = require("passport-local-mongoose"),
   userSchema = new Schema(
     {
       name: {
@@ -18,18 +19,14 @@ const mongoose = require("mongoose"),
       },
       email: {
         type: String,
-        // required: true,
-        // lowercase: true,
+        required: true,
+        lowercase: true,
         unique: true
       },
       zipCode: {
         type: Number,
-        // min: [1000, "Zip code too short"],
-        // max: 99999
-      },
-      password: {
-        type: String,
-        required: true
+        min: [1000, "Zip code too short"],
+        max: 99999
       },
       courses: [{ type: Schema.Types.ObjectId, ref: "Course" }],
       subscribedAccount: {
@@ -65,23 +62,8 @@ userSchema.pre("save", function(next) {
   }
 });
 
-userSchema.pre("save", function(next) {
-  let user = this;
-  bcrypt
-    .hash(user.password, 10)
-    .then(hash => {
-      user.password = hash;
-      next();
-    })
-    .catch(error => {
-      console.log(`Error in hashing password: ${error.message}`);
-      next(error);
-    });
+userSchema.plugin(passportLocalMongoose, {
+  usernameField: "email"
 });
-
-userSchema.methods.passwordComparison = function(inputPassword) {
-  let user = this;
-  return bcrypt.compare(inputPassword, user.password);
-};
 
 module.exports = mongoose.model("User", userSchema);
