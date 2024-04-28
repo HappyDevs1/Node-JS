4058956326ea0591acd3da44cdb3b4b78d5e16ed
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-04-28 12:38:02 +0200

    lesson_27

--- unit_6/lesson_27/recipe_app/routes/index.js
+++ unit_6/lesson_27/recipe_app/routes/index.js
@@ -5,12 +5,14 @@ const router = require("express").Router(),
   subscriberRoutes = require("./subscriberRoutes"),
   courseRoutes = require("./courseRoutes"),
   errorRoutes = require("./errorRoutes"),
-  homeRoutes = require("./homeRoutes");
+  homeRoutes = require("./homeRoutes"),
+  apiRoutes = require("./apiRoutes");
 
 router.use("/users", userRoutes);
 router.use("/subscribers", subscriberRoutes);
 router.use("/courses", courseRoutes);
+router.use("/api", apiRoutes);
 router.use("/", homeRoutes);
 router.use("/", errorRoutes);
 
-module.exports = router;  
+module.exports = router;
