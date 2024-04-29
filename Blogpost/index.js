dbefbe7f38a9ef382016c3d5d0daa7b0d40fa763
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-04-29 20:32:28 +0200

    minor changes

--- Blogpost/index.js
+++ Blogpost/index.js
@@ -1,20 +1,19 @@
-const express = require('express')
-const path = require("path")
-const app = new express()
-app.use(express.static('public'))
+ const express = require("express")
+ const path = require("path")
 
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "pages/index.html"))
-})
-app.get("/about", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "pages/about.html"))
-})
-app.get("/contact", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "pages/contact.html"))
-})
-app.get("/post", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "pages/post.html"))
-})
-app.listen(4000, ()=>{
- console.log('App listening on port 4000')
-})
+ const app = new express()
+ const  ejs = require("ejs")
+ app.set("view engine", "ejs")
+
+ app.get("/", (req, res) => {
+  res.render("index")
+ })
+ app.get("/about", (req, res) => {
+  res.render("about")
+ })
+ app.get("/post", (req, res) => {
+  res.render("post")
+ })
+ app.get("/contact", (req, res) => {
+  res.render("contact")
+ })
