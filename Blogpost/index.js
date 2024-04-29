72415a0d766ad7e69b4caf1e7ffdb376c96cdb7e
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-04-29 20:21:47 +0200

    chapter4

--- Blogpost/index.js
+++ Blogpost/index.js
@@ -1,6 +1,20 @@
-const express = require("express"),
-  app = new express()
+const express = require('express')
+const path = require("path")
+const app = new express()
+app.use(express.static('public'))
 
-  app.listen(4000, () => {
-    console.log("App is listening on port 4000")
-  })
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "pages/index.html"))
+})
+app.get("/about", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "pages/about.html"))
+})
+app.get("/contact", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "pages/contact.html"))
+})
+app.get("/post", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "pages/post.html"))
+})
+app.listen(4000, ()=>{
+ console.log('App listening on port 4000')
+})
