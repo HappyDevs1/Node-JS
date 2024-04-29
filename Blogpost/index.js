e7594d28b688c7b5410c45b86319d45f520dc05f
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-04-29 18:37:54 +0200

    creating a server

--- Blogpost/index.js
+++ Blogpost/index.js
@@ -1,6 +1,6 @@
-const http = require('http')
-const server = http.createServer((req, res) =>{
- console.log(req.url)
- res.end('Hello Node.js')
-})
-server.listen(3000)
+const express = require("express"),
+  app = new express()
+
+  app.listen(4000, () => {
+    console.log("App is listening on port 4000")
+  })
