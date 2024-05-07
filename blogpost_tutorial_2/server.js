c0accc38fc37cc1126d841377098a36fd333bac9
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-05-07 23:56:10 +0200

    connecting database

--- blogpost_tutorial_2/server.js
+++ blogpost_tutorial_2/server.js
@@ -1,1 +1,9 @@
 const mongoose = require("mongoose")
+
+mongoose
+  .connect("mongodb://localhost:27017/", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {console.log("Sucessfully connect to MongoDB")})
+  .catch((err) => {console.error("Connection error", err)})
